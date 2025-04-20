f65303f158387ff9e769c5aa34aa48e40b64b615
azbayor2 <hanharry2@gmail.com>
2025-04-21 06:41:10 +0900

    fixed bug

--- server/JWT/middleware.js
+++ server/JWT/middleware.js
@@ -1,27 +1,26 @@
 import jwt from 'jsonwebtoken';
-import makeToken from './token.js';
+import { makeToken } from './token.js';
 
 const secretKey = process.env.JWT_SECRET;
 
-export function CheckAndRemakeToken(req, res, next)
-{
-    const temp = res.headers.authorization;
+export function CheckAndRemakeToken(req, res, next) {
+  const temp = req.headers.authorization;
 
-    if(!temp)
-        return req.status(401).json({message: "No Token"});
+  if (!temp || !temp.startsWith('Bearer ')) {
+    return res.status(401).json({ message: "No Token" });
+  }
 
-    const token = temp.split(' ')[1];
+  const token = temp.split(' ')[1];
 
-    try{
-        const decoded = jwt.decode(token, secretKey);
-        req.user = decoded;
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
 
-        res.locals.newToken = makeToken(decoded);
+    res.locals.newToken = makeToken(decoded);
 
-        next();
-
-    } catch(err) {
-        console.error("error", err);
-        return res.status("403").json({message: "not verified token"});
-    }
+    next();
+  } catch (err) {
+    console.error("JWT Verify Error:", err);
+    return res.status(403).json({ message: "Not verified token" });
+  }
 }
