41914917dcaab0a7cdd0415e277b5505dd25c067
azbayor2 <hanharry2@gmail.com>
2025-05-12 17:17:28 +0900

    edited sql query

--- server/controllers/get_commentsController.js
+++ server/controllers/get_commentsController.js
@@ -16,44 +16,47 @@ export async function level1(req, res)  // 시도 단위
             [t_topleftx, t_bottomrightx, t_toplefty, t_bottomrighty]);
 
 
-        const loc_size = loc.length;   //좌표 안에 있는 위치의 개수
-        console.log("loc_size: " + loc_size);
-        let data = [];
+        const city_id_arr = loc.map(c=>c.id);
+        const holder = city_id_arr.map(i=>'?').join(', ');
 
-        for(let i =0; i<loc_size; i++)
+        const [db_res] = await db.query(
+            `SELECT * from (SELECT c.city_id, c.content AS comment, u.username AS posted_by, c.created_at AS posted_at, c.like_count, ROW_NUMBER() OVER
+            (PARTITION BY c.city_id ORDER BY c.like_count DESC) AS rn
+            FROM comments c 
+            JOIN user_db.users u ON c.user_id=u.id
+            WHERE c.city_id IN (${holder}) ) ranked
+            WHERE rn<=2
+            `, city_id_arr);
+        
+        let comment_info = {};
+            
+        for(const e of db_res)
         {
-            const [temp_comment] = await db.query('SELECT * from comments WHERE city_id=? ORDER BY like_count LIMIT 2', [loc[i].id]); //댓글 조회
-            const mapx = loc[i].lng;
-            const mapy = loc[i].lat;
-            const comments_size = temp_comment.length; //조회된 댓글 개수 (최대 2)
-            let comments_data = [];
-
-            for(let j=0; j<comments_size; j++)
-            {
-                const [username] = await userdb.query('SELECT username from users WHERE id=?', [temp_comment[j].user_id]); //사용자 username 조회
-                comments_data.push({
-                    comment: temp_comment[j].content,  //댓글 내용
-                    posted_by: username[0].username,  // username
-                    posted_at: temp_comment[j].created_at, //생성시간
-                    like_count: temp_comment[j].like_count //좋아요 개수
-                });
-            }
-
-            data.push({
-                mapx: mapx,
-                mapy: mapy,
-                comments_size: comments_size,
-                comments: comments_data
+            if(!comment_info[e.city_id]) comment_info[e.city_id] = [];
+            comment_info[e.city_id].push({
+                comment: e.comment,
+                posted_by: e.posted_by,
+                posted_at: e.posted_at,
+                like_count: e.like_count
             });
         }
 
-        const res_json = {
-            data_size: loc_size,
-            data: data
+        let data = [];
+
+        for(const l of loc)
+        {
+            data.push({
+                mapx: l.lng,
+                mapy: l.lat,
+                comments_size: comment_info[l.id].length,
+                comments: comment_info[l.id]
+            });
         }
 
-        res.status(201).json(res_json);
-        console.log("201: success")
+        res.status(201).json({
+            data_size: data.length,
+            data: data
+        });
 
     } catch(err) {
         console.error("error: ", err);
