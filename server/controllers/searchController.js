f088e2dc265f049e8a75c087a1f8704626bd45e4
azbayor2 <hanharry2@gmail.com>
2025-06-03 11:14:44 +0900

    added search sudo code

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -2,5 +2,18 @@ import db from '../db/appDb.js';
 import appdb from '../db/userDb.js';
 
 export async function search(req, res){
-    
+ 
+    try{
+
+        
+
+    } catch(err){
+        
+        console.error("error: ", err);
+        res.status(501).json({
+            error: err
+        });
+
+        return;
+    }
 }
