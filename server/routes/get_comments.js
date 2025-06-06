5389dfb00e60908485b87e6f2a8ab8e1d3182a4a
azbayor2 <hanharry2@gmail.com>
2025-06-07 00:31:14 +0900

    fixed bug

--- server/routes/get_comments.js
+++ server/routes/get_comments.js
@@ -13,11 +13,11 @@ router.get('/level2', level2);
 router.get('/level3', level3);
 router.get('/level4', level4);
 router.get('/level5', level5);
-router.get('/level4', get_all_level1);
-router.get('/level4', get_all_level2);
-router.get('/level4', get_all_level3);
-router.get('/level4', get_all_level4);
-router.get('/level4', get_all_level5);
+router.get('/get_all_level1', get_all_level1);
+router.get('/get_all_level2', get_all_level2);
+router.get('/get_all_level3', get_all_level3);
+router.get('/get_all_level4', get_all_level4);
+router.get('/get_all_level5', get_all_level5);
 
 export default router;
 
