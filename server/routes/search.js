c2f9ab024f376fa96f8d73c20658675f02da59e6
azbayor2 <hanharry2@gmail.com>
2025-06-06 17:17:39 +0900

    fixed

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import {
-  search
+  userSearch
 } from '../controllers/searchController.js'; 
 
 import { CheckAndRemakeToken } from '../JWT/middleware.js';
 
 const router = express.Router();
 
-router.get('/searchQuery', CheckAndRemakeToken, search);
+router.get('/searchQuery', CheckAndRemakeToken, userSearch);
 
 
 export default router;
