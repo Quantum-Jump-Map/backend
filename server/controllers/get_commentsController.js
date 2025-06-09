3d727330377c7ee36cffdafb604f451c6651c916
azbayor2 <hanharry2@gmail.com>
2025-06-09 15:05:24 +0900

    dfadfa

--- server/controllers/get_commentsController.js
+++ server/controllers/get_commentsController.js
@@ -157,8 +157,8 @@ export async function level2(req, res)   //시군구 단위
             
         for(const e of db_res)
         {
-            if(!comment_info[e.district_id]) comment_info[e.district_id] = [];
-            comment_info[e.district_id].push({
+            if(!comment_info[e.district_id_arr]) comment_info[e.district_id_arr] = [];
+            comment_info[e.district_id_arr].push({
                 comment: e.comment,
                 posted_by: e.posted_by,
                 posted_at: e.posted_at,
@@ -287,8 +287,8 @@ export async function level3(req, res)   //도로명+구 단위
             
         for(const e of db_res)
         {
-            if(!comment_info[e.road_id]) comment_info[e.road_id] = [];
-            comment_info[e.road_id].push({
+            if(!comment_info[e.road_id_arr]) comment_info[e.road_id_arr] = [];
+            comment_info[e.road_id_arr].push({
                 comment: e.comment,
                 posted_by: e.posted_by,
                 posted_at: e.posted_at,
@@ -298,8 +298,8 @@ export async function level3(req, res)   //도로명+구 단위
 
         for(const e of db_res_dong)
         {
-            if(!comment_info_dong[e.legal_dong_id]) comment_info_dong[e.legal_dong_id] = [];
-            comment_info_dong[e.legal_dong_id].push({
+            if(!comment_info_dong[e.dong_id_arr]) comment_info_dong[e.dong_id_arr] = [];
+            comment_info_dong[e.dong_id_arr].push({
                 comment: e.comment,
                 posted_by: e.posted_by,
                 posted_at: e.posted_at,
@@ -462,8 +462,8 @@ export async function level4(req, res)   //건물번호 단위
             
         for(const e of db_res)
         {
-            if(!comment_info[e.address_id]) comment_info[e.address_id] = [];
-            comment_info[e.address_id].push({
+            if(!comment_info[e.address_id_arr]) comment_info[e.address_id_arr] = [];
+            comment_info[e.address_id_arr].push({
                 comment: e.comment,
                 posted_by: e.posted_by,
                 posted_at: e.posted_at,
@@ -478,6 +478,7 @@ export async function level4(req, res)   //건물번호 단위
             data.push({
                 mapx: l.lng,
                 mapy: l.lat,
+                address_id: l.id,
                 comments_size: comment_info[l.id].length,
                 comments: comment_info[l.id]
             });
@@ -501,21 +502,24 @@ export async function get_all_level4(req, res)
 {
     try{
     
-        const {city_id, offset} = req.query;
+        const {address_id, offset} = req.query;
 
-        const [city_row] = await db.query(
+        const address_id_t = parseInt(address_id);
+        const offset_t = parseInt(offset);
+
+        const [address_row] = await db.query(
             `SELECT c.content AS comment, u.username AS posted_by, c.created_at AS posted_at, c.like_count
                 FROM comments c
                 JOIN user_db.users u ON u.id=c.user_id
-                WHERE c.city_id=?
+                WHERE c.address_id=?
                 ORDER BY c.like_count DESC
                 LIMIT 10
-                OFFSET ?`, [city_id, offset]);
+                OFFSET ?`, [address_id_t, offset_t]);
 
         res.status(201).json({
             data_size: city_row.length,
-            offset: offset+city_row.length,
-            comments: city_row
+            offset: offset_t+city_row.length,
+            comments: address_row
         });
         
     } catch(err) {
@@ -532,16 +536,69 @@ export async function get_all_level4(req, res)
 
 export async function level5(req, res)   //건물번호 단위
 {
-    try{
+    const {TopLeftX, TopLeftY, BottomRightX, BottomRightY} = req.query;
+        console.log(`자료형: ${typeof(TopLeftX)} ${typeof(TopLeftY)} ${typeof(BottomRightX)} ${typeof(BottomRightY)}\n`);
+        console.log(`값: ${TopLeftX} ${TopLeftY} ${BottomRightX} ${BottomRightY}`);
+        console.log(req.query);
+        const t_topleftx = Math.min(parseFloat(TopLeftX), parseFloat(BottomRightX));
+        const t_toplefty = Math.max(parseFloat(TopLeftY), parseFloat(BottomRightY));
+        const t_bottomrightx = Math.max(parseFloat(TopLeftX), parseFloat(BottomRightX));
+        const t_bottomrighty = Math.min(parseFloat(TopLeftY), parseFloat(BottomRightY));
+
+        const [loc] = await db.query('SELECT * from addresses WHERE lng BETWEEN ? AND ? AND lat BETWEEN ? AND ?', 
+            [t_topleftx, t_bottomrightx, t_bottomrighty, t_toplefty]);
+
+        if(loc.length==0){
+            res.status(201).json({
+                data_size: 0,
+                data: []
+            });
+
+            return;
+        }
+
+        const address_id_arr = loc.map(c=>c.id);
+        const holder = address_id_arr.map(i=>'?').join(', ');
+
+        const [db_res] = await db.query(
+            `SELECT * from (SELECT c.address_id, c.content AS comment, u.username AS posted_by, c.created_at AS posted_at, c.like_count, ROW_NUMBER() OVER
+            (PARTITION BY c.address_id ORDER BY c.like_count DESC) AS rn
+            FROM comments c 
+            JOIN user_db.users u ON c.user_id=u.id
+            WHERE c.address_id IN (${holder}) ) ranked
+            WHERE rn<=2
+            `, address_id_arr);
         
+        let comment_info = {};
+            
+        for(const e of db_res)
+        {
+            if(!comment_info[e.address_id]) comment_info[e.address_id] = [];
+            comment_info[e.address_id].push({
+                comment: e.comment,
+                posted_by: e.posted_by,
+                posted_at: e.posted_at,
+                like_count: e.like_count
+            });
+        }
 
-    } catch(err) {
-        console.error("error: ", err);
-        res.status(401).json({
-            error: "server error"
+        let data = [];
 
+        for(const l of loc)
+        {
+            data.push({
+                mapx: l.lng,
+                mapy: l.lat,
+                address_id: l.id,
+                comments_size: comment_info[l.id].length,
+                comments: comment_info[l.id]
+            });
+        }
+
+        res.status(201).json({
+            data_size: data.length,
+            data: data
         });
-    }
 }
 
 export async function get_all_level5(req, res)
