03b62ed1c63950098586fbd5ec67269c77996b6f
azbayor2 <107553591+azbayor2@users.noreply.github.com>
2025-06-11 11:19:30 +0000

    fixed event db schema

--- db/app/init.sql
+++ db/app/init.sql
@@ -124,14 +124,18 @@ CREATE DATABASE IF NOT EXISTS event_db;
 USE event_db;
 
 CREATE TABLE IF NOT EXISTS festivals (
-  content_id VARCHAR(20) PRIMARY KEY, -- TourAPI 고유값
+  content_id INT(20) PRIMARY KEY, -- TourAPI 고유값
   title VARCHAR(255) NOT NULL,
+  city_id INT NOT NULL,
+  district_id INT NOT NULL,
+  road_id INT,
+  legal_dong_id INT,
   address_id INT NOT NULL,
+  is_road BOOLEAN DEFAULT 1
   event_start_date DATE NOT NULL,
   event_end_date DATE NOT NULL,
   first_image VARCHAR(255),
   first_image2 VARCHAR(255),
   mapx DOUBLE NOT NULL,
-  mapy DOUBLE NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  mapy DOUBLE NOT NULL
 );
