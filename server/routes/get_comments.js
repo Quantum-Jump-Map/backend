97b1f34bdce7c1d881e203b7eff99aa10ae385f4
azbayor2 <hanharry2@gmail.com>
2025-06-25 12:46:56 +0900

    dafdf

--- server/routes/get_comments.js
+++ server/routes/get_comments.js
@@ -10,15 +10,15 @@ import {CheckAndRemakeToken} from '../JWT/middleware.js'
 const router = express.Router();
 
 router.get('/level1', CheckAndRemakeToken, level1);
-router.get('/level2', level2);
-router.get('/level3', level3);
-router.get('/level4', level4);
-router.get('/level5', level5);
-router.get('/get_all_level1', get_all_level1);
-router.get('/get_all_level2', get_all_level2);
-router.get('/get_all_level3', get_all_level3);
-router.get('/get_all_level4', get_all_level4);
-router.get('/get_all_level5', get_all_level5);
+router.get('/level2', CheckAndRemakeToken, level2);
+router.get('/level3', CheckAndRemakeToken, level3);
+router.get('/level4', CheckAndRemakeToken, level4);
+router.get('/level5', CheckAndRemakeToken, level5);
+router.get('/get_all_level1', CheckAndRemakeToken, get_all_level1);
+router.get('/get_all_level2', CheckAndRemakeToken, get_all_level2);
+router.get('/get_all_level3', CheckAndRemakeToken, get_all_level3);
+router.get('/get_all_level4', CheckAndRemakeToken, get_all_level4);
+router.get('/get_all_level5', CheckAndRemakeToken, get_all_level5);
 
 export default router;
 
